81e75d732c6a18fb379fb71416f54f790fe16ca3
ondrasouk <ondrasouk@seznam.cz>
2021-12-14 15:31:21 +0100

    Clean the display code

--- src/main.c
+++ src/main.c
@@ -23,12 +23,24 @@
 #include <string.h>
 
 
+/* Definitions -------------------------------------------------------*/
 #define DISPLAY_ROWS 2
 #define MENU_TEXT_SIZE_D 10
 #define MENU_TEXT_SHIFT_TIME_USEC 150000
 #define MENU_ITEM_SIZE_D 5
 #define TIM2_OVERFLOW_TIME 4096
+#define MENU_TEXT_SHIFT_NUM MENU_TEXT_SHIFT_TIME_USEC / TIM2_OVERFLOW_TIME
+
+#if !defined(ARRAY_SIZE)
+    #define ARRAY_SIZE(x) (sizeof((x)) / sizeof((x)[0]))
+#endif
+#define CURSOR_POS (menu_pos + scroll_pos)
+
+/*--------------------------------------------------------------------*/
 /* Globals -----------------------------------------------------------*/
+/*--------------------------------------------------------------------*/
+
+/* Custom Characters -------------------------------------------------*/
 // Custom character definition using https://omerk.github.io/lcdchargen/
 const uint8_t customChar[] = {
     0b11000, 0b11100, 0b10110, 0b10011, 0b10011, 0b10110, 0b11100,
@@ -46,21 +58,30 @@ const uint8_t customChar[] = {
     0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111,
     0b11111, // 6
 };
-#define MENU_TEXT_SHIFT_NUM MENU_TEXT_SHIFT_TIME_USEC / TIM2_OVERFLOW_TIME
-#if !defined(ARRAY_SIZE)
-    #define ARRAY_SIZE(x) (sizeof((x)) / sizeof((x)[0]))
-#endif
-#define CURSOR_POS (menu_pos + scroll_pos)
 
-// static uint8_t MENU_TEXT_SIZE = MENU_TEXT_SIZE_D;
+/* Characters for type -----------------------------------------------*/
+// "\xHH" for entering plain hexadecimal values (0-9 is user defined symbols)
+const char type_suffix[5] = "  %ms";
 
+/* Structures definitions --------------------------------------------*/
 typedef struct {
-    const uint8_t type; // 1 - plain; 2 - with "%"; 3 - with "m"
+    const uint8_t type; // 1 - plain; 2 - with "%"; 3 - with "m"; 4 - with "s"; higher is character menu items
     const uint8_t limit_min;
     const uint8_t step;
     const uint8_t limit_max;
     uint8_t *value;
 } item_uint8;
+
+
+/* Menu --------------------------------------------------------------*/
+
+uint8_t irrigation_mode = 0; // Variable with what menu to display
+
+const char *menu_head_items[] = {
+    "smart",
+    " drip",
+    "const",
+};
 
 const char *smart_menu[] = {
     "Irrigation mode:",
@@ -74,6 +95,7 @@ uint8_t irrigation_level = 50;
 uint8_t save_water_level = 20;
 
 const item_uint8 smart_menu_items[] = {
+    {5, 0, 1, ARRAY_SIZE(menu_head_items)-1, &irrigation_mode},
     {1, 1, 1, 14, &days_away},
     {2, 0, 10, 100, &irrigation_level},
     {2, 0, 20, 100, &save_water_level},
@@ -91,6 +113,7 @@ uint8_t irrigation_min = 30;
 uint8_t irrigation_duration_min = 20;
 
 const item_uint8 drip_menu_items[] = {
+    {5, 0, 1, ARRAY_SIZE(menu_head_items)-1, &irrigation_mode},
     {2, 10, 10, 100, &irrigation_max},
     {2, 0, 10, 100, &irrigation_min},
     {3, 0, 1, 255, &irrigation_duration_min},
@@ -108,16 +131,11 @@ uint8_t irrigation_hysterezis = 20;
 uint8_t irrigation_wait_min = 100;
 
 const item_uint8 const_menu_items[] = {
+    {5, 0, 1, ARRAY_SIZE(menu_head_items)-1, &irrigation_mode},
     {2, 1, 1, 14, &irrigation_level},
     {2, 10, 10, 100, &irrigation_hysterezis},
     {3, 0, 1, 255, &irrigation_duration_min},
     {3, 0, 1, 255, &irrigation_wait_min},
-};
-
-const char *menu_head_items[] = {
-    "smart",
-    " drip",
-    "const",
 };
 
 const char **mode_menu_pointers[] = {
@@ -138,24 +156,69 @@ const uint8_t mode_menu_lenght[] = {
     ARRAY_SIZE(const_menu),
 };
 
-const char type_suffix[4] = "  %m";
+const char **menu_character_values_pointers[] = {
+    menu_head_items,
+};
+
+const uint8_t menu_character_values_lenght[] = {
+    ARRAY_SIZE(menu_head_items),
+};
+
+/* Global variables --------------------------------------------------*/
 
 uint16_t ADC_key_value = 65535;
+
 uint8_t menu_pos = 0;
 uint8_t scroll_pos = 0;
 
 uint8_t slice_start = 0;
 const char *shift_string;
-char shift_buffer[MENU_TEXT_SIZE_D + 1] = "         \x7e"; // last character is left arrow + end of string
+char shift_buffer[MENU_TEXT_SIZE_D + 1] = "         \x7e"; // last characters is left arrow + end of string
 uint8_t scroll_pos_shift = 255;
 
 char value_disp[MENU_ITEM_SIZE_D + 1] = "     ";
 
 uint8_t TIM2_flag = 0;
 
-uint8_t irrigation_mode = 0;
-
+/*--------------------------------------------------------------------*/
 /* Function definitions ----------------------------------------------*/
+/*--------------------------------------------------------------------*/
+
+void cyclic_inc(uint8_t *x, const uint8_t lim_min, const uint8_t lim_max)
+{
+    uint8_t value = *x;
+    *x = value + 1;
+    if((*x > lim_max)|((*x - value) != 1)){
+        *x = lim_min;
+    }
+}
+
+void cyclic_dec(uint8_t *x, const uint8_t lim_min, const uint8_t lim_max)
+{
+    uint8_t value = *x;
+    *x = value - 1;
+    if((*x < lim_min)|((value - *x) != 1)){
+        *x = lim_max;
+    }
+}
+
+void limited_inc(uint8_t *x, const uint8_t lim, const uint8_t step)
+{
+    uint8_t value = *x;
+    *x = value + step;
+    if((*x > lim)|((*x - value) != step)){
+        *x = lim;
+    }
+}
+
+void limited_dec(uint8_t *x, const uint8_t lim, const uint8_t step)
+{
+    uint8_t value = *x;
+    *x = value - step;
+    if((*x < lim)|((value - *x) != step)){
+        *x = lim;
+    }
+}
 
 void slice_str(const char *str, char *buffer, uint8_t start)
 {
@@ -165,22 +228,32 @@ void slice_str(const char *str, char *buffer, uint8_t start)
 void itoa_menu_item(int value, char *str, uint8_t type)
 {
     strcpy(str, "     "); // Clear output buffer
-    if(value < 10){
-        itoa(value, str + MENU_ITEM_SIZE_D - 2, 10);
-    }
-    else if(value < 100){
-        itoa(value, str + MENU_ITEM_SIZE_D - 3, 10);
-    }
-    else if(value < 1000){
-        itoa(value, str + MENU_ITEM_SIZE_D - 4, 10);
-    }
-    else if(value < 10000){
-        itoa(value, str + MENU_ITEM_SIZE_D - 5, 10);
+    if(type < ARRAY_SIZE(type_suffix)){
+        if(value < 10){
+            itoa(value, str + MENU_ITEM_SIZE_D - 2, 10);
+        }
+        else if(value < 100){
+            itoa(value, str + MENU_ITEM_SIZE_D - 3, 10);
+        }
+        else if(value < 1000){
+            itoa(value, str + MENU_ITEM_SIZE_D - 4, 10);
+        }
+        else if(value < 10000){
+            itoa(value, str + MENU_ITEM_SIZE_D - 5, 10);
+        }
+        else{
+            itoa(value, str, 10);
+        }
+        str[4] = type_suffix[type];
     }
     else{
-        itoa(value, str, 10);
+        if((type - ARRAY_SIZE(type_suffix)) < ARRAY_SIZE(menu_character_values_pointers)){
+            strcpy(str ,menu_character_values_pointers[type-ARRAY_SIZE(type_suffix)][value]);
+        }
+        else{
+            str = "error";
+        }
     }
-    str[4] = type_suffix[type];
 }
 
 void TIM2_routine() 
@@ -207,60 +280,29 @@ void TIM2_routine()
     }
 }
 
-void menu_print(const char *menu_entries[])
+void menu_line_print(const char *menu_entries[], uint8_t line)
 {
-    lcd_clrscr();
-    lcd_gotoxy(1,0);
-    lcd_gotoxy(1, 0); // Fix some unknown problem
-    scroll_pos_shift = 255;
-    if(strlen(menu_entries[menu_pos]) > MENU_TEXT_SIZE_D){
+    lcd_gotoxy(1,line);
+    if(strlen(menu_entries[menu_pos + line]) > MENU_TEXT_SIZE_D){
         slice_start = 0;
-        slice_str(menu_entries[menu_pos], shift_buffer, 0);
+        slice_str(menu_entries[menu_pos + line], shift_buffer, 0);
         lcd_puts(shift_buffer);
-        if(scroll_pos == 0){
-            shift_string = menu_entries[menu_pos];
-            scroll_pos_shift = 0;
+        if(scroll_pos == line){
+            shift_string = menu_entries[menu_pos + line];
+            scroll_pos_shift = line;
         }
     }
     else{
-        lcd_puts(menu_entries[menu_pos]);
+        lcd_puts(menu_entries[menu_pos + line]);
     }
-    lcd_gotoxy(1,1);
-    if(strlen(menu_entries[menu_pos+1]) > MENU_TEXT_SIZE_D){
-        slice_start = 0;
-        slice_str(menu_entries[menu_pos+1], shift_buffer, 0);
-        lcd_puts(shift_buffer);
-        if(scroll_pos == 1){
-            shift_string = menu_entries[menu_pos+1];
-            scroll_pos_shift = 1;
-        }
-    }
-    else{
-        lcd_puts(menu_entries[menu_pos+1]);
-    }
-    lcd_gotoxy(0,scroll_pos);
-    lcd_putc(0); //Print cursor >
-    if(menu_pos == 0){
-        lcd_gotoxy(11, 0);
-        lcd_puts(menu_head_items[irrigation_mode]);
-        lcd_gotoxy(11, 1);
-        itoa_menu_item(*mode_menu_items[irrigation_mode][menu_pos].value, value_disp, mode_menu_items[irrigation_mode][menu_pos].type);
-        lcd_puts(value_disp);
-    }
-    else{
-        lcd_gotoxy(11, 0);
-        itoa_menu_item(*mode_menu_items[irrigation_mode][menu_pos-1].value, value_disp, mode_menu_items[irrigation_mode][menu_pos-1].type);
-        lcd_puts(value_disp);
-        lcd_gotoxy(11, 1);
-        itoa_menu_item(*mode_menu_items[irrigation_mode][menu_pos].value, value_disp, mode_menu_items[irrigation_mode][menu_pos].type);
-        lcd_puts(value_disp);
-    }
-    
+    lcd_gotoxy(11, line);
+    itoa_menu_item(*mode_menu_items[irrigation_mode][menu_pos+line].value, value_disp, mode_menu_items[irrigation_mode][menu_pos+line].type);
+    lcd_puts(value_disp);
 }
 
 void menu(uint8_t key_press)
 {
-    if(key_press == 3){
+    if(key_press == 3){ // UP Key
         scroll_pos++;
         if((menu_pos < mode_menu_lenght[irrigation_mode]-2) & (scroll_pos == 2)){
             menu_pos++;
@@ -270,7 +312,7 @@ void menu(uint8_t key_press)
             scroll_pos--;
         }
     }
-    if(key_press == 4){
+    if(key_press == 4){ // DOWN Key
         scroll_pos--;
         if((menu_pos > 0) & (scroll_pos == 255)){
             menu_pos--;
@@ -280,39 +322,30 @@ void menu(uint8_t key_press)
             scroll_pos++;
         }
     }
-    item_uint8 menu_item = mode_menu_items[irrigation_mode][CURSOR_POS - 1];
-    uint8_t item_value;
-    if(key_press == 5){
-        if(CURSOR_POS == 0){
-            irrigation_mode++;
-            if(irrigation_mode > ARRAY_SIZE(mode_menu_lenght) - 1){
-                irrigation_mode = 0;
-            }
+    item_uint8 menu_item = mode_menu_items[irrigation_mode][CURSOR_POS];
+    if(key_press == 5){ // RIGHT Key
+        if(menu_item.type >= ARRAY_SIZE(type_suffix)){
+            cyclic_inc(menu_item.value, menu_item.limit_min, menu_item.limit_max);
         }
         else{
-            item_value = *menu_item.value;
-            *menu_item.value += menu_item.step;
-            if((*menu_item.value > menu_item.limit_max)|((*menu_item.value - item_value) != menu_item.step)){
-                *menu_item.value = menu_item.limit_max;
-            }
+            limited_inc(menu_item.value, menu_item.limit_max, menu_item.step);
         }
     }
-    if(key_press == 2){
-        if(CURSOR_POS == 0){
-            irrigation_mode--;
-            if(irrigation_mode == 255){
-                irrigation_mode = ARRAY_SIZE(mode_menu_lenght) - 1;
-            }
+    if(key_press == 2){  // LEFT Key
+        if(menu_item.type >= ARRAY_SIZE(type_suffix)){
+            cyclic_dec(&irrigation_mode, menu_item.limit_min, menu_item.limit_max);
         }
         else{
-            item_value = *menu_item.value;
-            *menu_item.value -= menu_item.step;
-            if((*menu_item.value < menu_item.limit_min)|((item_value - *menu_item.value) != menu_item.step)){
-                *menu_item.value = menu_item.limit_min;
-            }
+            limited_dec(menu_item.value, menu_item.limit_min, menu_item.step);
         }
     }
-    menu_print(mode_menu_pointers[irrigation_mode]);
+    lcd_clrscr();
+    lcd_gotoxy(1,0); // Fix some unknown problem
+    scroll_pos_shift = 255;
+    menu_line_print(mode_menu_pointers[irrigation_mode], 0);
+    menu_line_print(mode_menu_pointers[irrigation_mode], 1);
+    lcd_gotoxy(0,scroll_pos);
+    lcd_putc(0); // Print cursor >
     shift_buffer[MENU_TEXT_SIZE_D - 1] = 0x7e;
 }
 
@@ -362,7 +395,6 @@ int main(void)
     // Set prescaler to 16 ms and enable overflow interrupt
     TIM2_overflow_16ms();
     TIM2_overflow_interrupt_enable();
-    
     
     // Initialize UART to asynchronous, 8N1, 9600
     uart_init(UART_BAUD_SELECT(9600, F_CPU));
